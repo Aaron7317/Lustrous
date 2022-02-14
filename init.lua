b3727ddec9d15e4718b25dc9c2d32acbd2b2defb
Aaron7317 <aaron.boyd.org@gmail.com>
2022-02-14 00:19:54 -0800

    added phosphophyllite ore

--- init.lua
+++ init.lua
@@ -1,1 +1,16 @@
--- register nodes
+
+-- phosphophyllite
+minetest.register_node("lustrous:stone_with_phosphophyllite", {
+	description = "ore_info",
+	tiles = {"default_stone.png^phosphophyllite_ore.png"},
+	groups = {cracky = 1}
+})
+
+minetest.register_ore({
+	ore_type = "scatter",
+	ore = "lustrous:stone_with_phosphophyllite",
+	wherein = "default_stone",
+	clust_scarcity = "1000",
+	clust_num_ores = "3",
+	y_max = "-256",
+})
